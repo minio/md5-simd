962a78b3026462f23899c4ffc3c1551adb57f31d
frankw <fwessels@xs4all.nl>
2020-04-13 15:39:28 -0700

    Concurrent MD5 simulator and verifier

--- md5-server16_test.go
+++ md5-server16_test.go
@@ -3,9 +3,16 @@ package md5simd
 import (
 	"bytes"
 	"crypto/md5"
+	"github.com/remeh/sizedwaitgroup"
 	"fmt"
 	"hash"
+	"runtime"
+	"sync"
 	"testing"
+	"math/rand"
+	"time"
+	"strings"
+	"strconv"
 )
 
 func TestGolden16(t *testing.T) {
@@ -158,5 +165,65 @@ func BenchmarkCryptoMd5(b *testing.B) {
 	})
 	b.Run("2MB", func(b *testing.B) {
 		benchmarkCryptoMd5(b, 2*1024*1024)
+	})
+}
+
+func testMd5Simulator(t *testing.T, server *Md5Server16) {
+
+	rand.Seed(time.Now().UnixNano())
+	verifier := make(map[string]string)
+
+	mu := sync.Mutex{}
+
+	swg := sizedwaitgroup.New(24)
+	for _i := 0; _i < 1000; _i++ {
+		swg.Add()
+		go func(i int) {
+			defer swg.Done()
+			h := NewMd5_x16(server)
+			mbs := 10 + rand.Intn(100)
+			h.Write(bytes.Repeat([]byte{0x61 + byte(i)}, mbs*1024*1024))
+			digest := fmt.Sprintf("%x", h.Sum([]byte{}))
+			mu.Lock()
+			verifier[fmt.Sprintf("%d-%d", i, mbs)] = digest
+			mu.Unlock()
+		}(_i)
+	}
+
+	swg.Wait()
+
+	fmt.Printf("Verifying %d entries...\n", len(verifier))
+
+	swg = sizedwaitgroup.New(runtime.NumCPU())
+
+	for _input, _digest := range verifier {
+
+		swg.Add()
+		go func(input, digest string) {
+			defer swg.Done()
+
+			p := strings.Split(input, "-")
+			i, _ := strconv.Atoi(p[0])
+			mbs, _ := strconv.Atoi(p[1])
+
+			h := md5.New()
+			h.Write(bytes.Repeat([]byte{0x61 + byte(i)}, mbs*1024*1024))
+			d := fmt.Sprintf("%x", h.Sum([]byte{}))
+
+			if digest != d {
+				t.Errorf("testMd5Simulator[%s], got %s, want %s", input, digest, d)
+			}
+		}(_input, _digest)
+	}
+	swg.Wait()
+
+}
+
+func TestMd5Simulator(t *testing.T) {
+
+	server := NewMd5Server16()
+
+	t.Run("", func(t *testing.T) {
+		testMd5Simulator(t, server)
 	})
 }
